edd7269c39a38dd07c493e2f996c003273bbe39f
Sungbae <9uiopa@gmail.com>
2023-11-30 00:24:49 +0900

    Update 주식가격.py

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -12,3 +12,18 @@ def solution(prices):
                 break
         answer.append(time)
     return answer
+
+
+# stack 이용 -> O(n) :
+def solution2(prices):
+    stack = []
+    answer = [0] * len(prices)
+    for i in range(len(prices)):
+        if stack != []:
+            while stack != [] and stack[-1][1] > prices[i]:
+                past, _ = stack.pop()
+                answer[past] = i - past
+        stack.append([i, prices[i]])
+    for i, s in stack:
+        answer[i] = len(prices) - 1 - i
+    return answer
