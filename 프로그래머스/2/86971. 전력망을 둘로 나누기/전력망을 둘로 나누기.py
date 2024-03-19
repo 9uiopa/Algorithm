588c5dd4c10c8c3e4691840b609da1a4da96dbcd
Sungbae <9uiopa@gmail.com>
2024-03-19 22:01:38 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 47.78 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,30 +1,32 @@
 import sys
-def dfs(graph, visited, node):
-    count = 1
-    visited[node] = True
-    for neighbor in graph[node]:
-        if not visited[neighbor]:
-            count += dfs(graph, visited, neighbor)
-    return count
-
+from collections import deque
 def solution(n, wires):
-    min_diff = sys.maxsize
-    for i in range(len(wires)): # 전선 끊어진 모든 경우 
-        # 전력망 그래프 생성
-        graph = [[] for _ in range(n+1)] # 0 ~ n 까지 존재하도록
-        for j, (a, b) in enumerate(wires):
-            if i == j:
-                continue  # 전선 끊기 처리 
-            graph[a].append(b)
-            graph[b].append(a)
+    def get_diff(wires_sep): # 송전탑 개수의 차이 반환
+        visited = set()
+        count = 1
+        q = deque()
+        q.append(wires_sep[0][0])
+        visited.add(wires_sep[0][0])    
+        while q:
+            node = q.popleft()
+            for i, (a,b) in enumerate(wires_sep):
+                if a == node and b not in visited:
+                    q.append(b)
+                    visited.add(b)
+                    count+=1
+                elif b == node and a not in visited:
+                    q.append(a)
+                    visited.add(a)
+                    count+=1
+        return abs(n-(count*2)) # 개수의 차이
+    
+    ans = sys.maxsize
+    
+    for i in range(len(wires)):
+        wires_sep = wires[:i] + wires[i+1:]  # 전선이 하나 끊긴 상태 : wires_sep
+        
+        d = get_diff(wires_sep)
+        ans = min(d,ans) 
 
-        # 전력망을 두 개로 분할하여 송전탑 개수 계산
-        visited = [False] * (n+1)
-        count1 = dfs(graph, visited, wires[0][0])  # 첫 번째 전력망
-        count2 = n - count1  # 두 번째 전력망
+    return ans
 
-        # 송전탑 개수의 차이 계산하여 최소값 갱신
-        diff = abs(count1 - count2)
-        min_diff = min(min_diff, diff)
-
-    return min_diff
