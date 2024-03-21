ac739028105d029bb3f649c029349da9cb5b8e1d
Sungbae <9uiopa@gmail.com>
2024-03-21 18:15:10 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,8 +1,8 @@
 def solution(brown, yellow):
-    sum = (brown-4)/2
-    for num in range(1, int(sum/2)+1):
-        h = num
-        w = sum - h
+    s = (brown-4)/2
+    for num in range(1, int(s/2)+1):
+        h = num # yellow의 높이
+        w = s - h #yellow의 너비
         if w*h == yellow:
             return [w+2,h+2]
     
