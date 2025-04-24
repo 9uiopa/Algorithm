42b65c8ae821e2bc4f43038d11ded6f9c8131f01
Sungbae <9uiopa@gmail.com>
2025-04-24 16:26:18 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 59 ms, Memory: 50,304 KB -BaekjoonHub

---
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.py
@@ -0,0 +1,22 @@
+for test_case in range(1, 11):
+    pw_len = int(input())
+    data = input().split()
+    com_len = int(input())
+    command = input().split()
+    i = 0
+    while i < len(command):
+        if command[i]=='I':
+            x = int(command[i+1])
+            y = int(command[i+2])
+            data = data[:x] + command[i+3 : i+3+y] + data[x:]
+            i+=3+y
+        elif command[i]=='D':
+            x = int(command[i+1])
+            y = int(command[i+2])
+            data = data[:x] + data[x+y:]
+            i+=3
+        else:
+            i+=1
+    print("#" + str(test_case) + " " + " ".join(data[:10]))
+    
+    
