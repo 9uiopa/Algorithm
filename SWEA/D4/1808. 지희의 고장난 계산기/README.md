65151247e558e849076bed0954475e2f399f2f41
Sungbae <9uiopa@gmail.com>
2025-05-19 22:59:10 +0900

    [D4] Title: 지희의 고장난 계산기, Time: 97 ms, Memory: 60,160 KB -BaekjoonHub

---
+++ SWEA/D4/1808. 지희의 고장난 계산기/README.md
@@ -0,0 +1,15 @@
+# [D4] 지희의 고장난 계산기 - 1808 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV4yC3pqCegDFAUx) 
+
+### 성능 요약
+
+메모리: 60,160 KB, 시간: 97 ms, 코드길이: 1,760 Bytes
+
+### 제출 일자
+
+2025-05-19 22:59
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1808. 지희의 고장난 계산기/지희의 고장난 계산기.py
@@ -0,0 +1,61 @@
+T = int(input())
+
+def get_factor(num): # 인수 구하기
+    factors = []
+    for i in range(2, int(num**(0.5))+1):
+        if num % i == 0 and num != i and can_make(i) : # i가 인수이고 문자열로 만들 수 있을때.  
+            small = i
+            big = num // i
+            factors.append((small,big))
+    return factors
+          
+def dfs(small, big): # 인수를 만들어낼 수 있는지 확인하고 수행 횟수 반환
+    global ans
+    flag = can_make(big)
+    result = float('inf')
+    if flag :
+        if small * big == x: # 최상단 함수일때,
+            ans = min(ans,  len(str(small))+1+flag)
+        else :  return len(str(small))+1+flag
+    else : # can not make 'big'
+        for s,b in get_factor(big):
+            temp = dfs(s,b)
+            if temp :
+                if small * big == x: # 최상단 함수일때,
+                    ans = min(ans,  len(str(small))+1+temp)
+                else :
+                    return len(str(small))+1+ temp
+    return 0
+
+def can_make(num):  # 문자열 합치기로 만들 수 있는지 판단, 가능하면 문자열크기 반환
+    s = str(num)
+    temp = 0
+    for c in s:
+        if int(c) in nums : temp+=1
+    if temp == len(s) : 
+        return temp
+    return 0
+    
+for test_case in range(1, T + 1):
+    idx = list(map(int, input().split()))
+    nums = []
+    for i in range(0,10):
+        if idx[i] : nums.append(i)    
+    x = int(input())
+    ans = float('inf')
+
+    # x를 문자열로 바로 만들수 있는지 확인. 
+    if can_make(x):
+        ans = can_make(x) +1
+        print(f"#{test_case} {ans}")
+        continue
+            
+    # 인수 찾기 + 횟수찾기
+    factors = get_factor(x)
+    for small, big in factors:
+        dfs(small, big)
+    if ans == float('inf'):
+        ans = -1
+    else : ans+=1 # 마지막 =
+    print(f"#{test_case} {ans}")
+    
